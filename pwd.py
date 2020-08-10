22b8415353a96ea1961585e60dc9a5ce0b42a91b
Charles Bonoan <cbonoan@prosoft-technology.com>
2020-08-10 16:30:44 -0700

    Starting making frames

--- pwd.py
+++ pwd.py
@@ -1,10 +1,40 @@
-#! python3
-
+import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
 import os
 
-def menuWindow():
+class SeaofBTCapp(tk.Tk):
+    def __init__(self):
+        
+        tk.Tk.__init__(self)
+        container = tk.Frame(self)
+
+        container.pack(side="top", fill="both", expand = True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (PasswordPage, GeneratorPage, VaultPage):
+
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+class PasswordPage():
+    pass
+class GeneratorPage():
+    pass
+class VaultPage():
     pass
 
 def checkPwd(pwd):
@@ -15,19 +45,19 @@ def checkPwd(pwd):
 
     if pwd == masterPwd:
         messagebox.showinfo("Password Input", "Correct!")
-        menuWindow()
+        menuFrame()
     else:
         messagebox.showinfo("Password Input", "Incorrect!")
-          
 
 if __name__ == "__main__":
-    
+
     gui = Tk()
     gui.geometry('500x500')
     gui.title('Password Vault')
+    gui.iconbitmap()
     gui.configure(bg='#81c6e3')
-    
 
+    '''
     field =  Entry(gui, bg='white', show="*")
     field.pack()
     field.focus_set()
@@ -35,6 +65,7 @@ if __name__ == "__main__":
     buttonEnter = Button(gui, text='Submit', command = lambda: checkPwd(field.get()))
     buttonEnter.pack()
     buttonEnter.focus_set()
-    
+    '''
 
     gui.mainloop()
+    
